bee1bbfd1ed43c79389dc885eab4bed392210db0
rektide <rektide@voodoowarez.com>
2015-10-19 20:25:26 -0400

    Prepare for json-lidification

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,23 +3,31 @@ var
   fetch= require( "isomorphic-fetch"),
   fs= require( "fs-promise")
 
-gulp.task( "source", function(){
+
+function sourceFile(){
 	var
 	  file= process.argv[ 1]|| process.env[ "SCHEMAORG_RDFA"]|| "schema.rdfa",
 	  branch= process.env[ "SCHEMAORG_BRANCH"]
-	return fs.exists( "./"+ file).then( function(exists){
-		if( exists){
-			return
-		}
-		return fetch("https://raw.githubusercontent.com/schemaorg/schemaorg/sdo-phobos/data/schema.rdfa").then( function(response){
-			if( response.status >= 400){
-				throw new Error( "Incorrect response from Github:", response.status)
-			}
-			return fs.writeFile( "./"+ file, response.text())
+	return fs
+		.readFile( "./"+ file)
+		.catch( function(){
+			return fetch( "https://raw.githubusercontent.com/schemaorg/schemaorg/sdo-phobos/data/schema.rdfa").then( function( response){
+				if( response.status >= 400){
+					throw new Error( "Incorrect response from Github:", response.status)
+				}
+				var text= response.text()
+				return fs.writeFile( "./"+ file, text).then(function(){ return text})
+			})
 		})
-	})
-})
+}
 
-gulp.task( "jsonld", ["source"], function(){
-	
-})
+function jsonld(){
+	sourceFile.then( function(source){
+	})
+}
+
+gulp.task( "source", sourceFile)
+gulp.task( "jsonld", jsonld)
+gulp task( "default", ["jsonld"])
+module.exports.sourceFile= sourceFile
+module.exports.jsonld= jsonld
